7d799743cae1b5230175b188545e009224591093
Alish Giri <alishgiri.92@gmail.com>
2020-05-11 13:47:52 +0545

    monsters CRUD functionality completed

--- routes/monsters.js
+++ routes/monsters.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const pool = require("../db");
+const { request } = require("express");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -19,7 +20,53 @@ router.get("/:id", async (req, res, next) => {
     ]);
     res.json(result.rows);
   } catch (e) {
-    next(e);
+    return next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, personality } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO monsters(name, personality) VALUES($1, $2)",
+      [name, personality]
+    );
+    res.json(result);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const fields = [];
+  const { id } = req.params;
+  const keys = ["name", "personality"];
+
+  keys.forEach((key) => {
+    if (req.body[key]) fields.push(key);
+  });
+
+  fields.forEach(async (field, index) => {
+    try {
+      await pool.query(`UPDATE monsters SET ${field}=($1) WHERE id=($2)`, [
+        req.body[field],
+        id,
+      ]);
+      if (fields.length === index + 1) res.redirect("/api/monsters");
+    } catch (e) {
+      console.log(e);
+      return next(e);
+    }
+  });
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("DELETE FROM monsters WHERE id=($1)", [id]);
+    res.send(result);
+  } catch (e) {
+    return next(e);
   }
 });
 
